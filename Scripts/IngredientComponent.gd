10fab89ee713f7a8bc3ea9416be7cc4d6c9442f2
Tyler Walton <tyler.walton1452@gmail.com>
2023-05-12 15:07:27 -0500

    Ability to Stack anything inside some boxes

--- Scripts/IngredientComponent.gd
+++ Scripts/IngredientComponent.gd
@@ -15,14 +15,18 @@ func hold_item(item: Node3D):
 			item.position = get_child(-2).position + stacking_spacing
 		else:
 			item.position = Vector3.ZERO
-	
+		
 
 func _on_interactable_component_interacted(_node : InteractableComponent, player : Player):
-	# Player Taking Item
+	# Player Taking Item from this Holder
 	if not player.holder_component.is_holding_item():
+		# Holder has something to give
 		if is_holding_item():
 			player.holder_component.hold_item(get_held_item())
-	# Take Player's item if it matches
+			released_holding.emit(get_held_item())
+	# Taking Player's item no matter what
+	elif ingredient_scene == null:
+		hold_item(player.holder_component.get_held_item())
+	# Taking Player's item if it matches
 	elif player.holder_component.get_held_item().scene_file_path == ingredient_scene.resource_path:
 		hold_item(player.holder_component.get_held_item())
-		released_holding.emit(get_held_item())
