cc195688de7417c31555c8354197a46f2fcb5a5f
Tyler Walton <tyler.walton1452@gmail.com>
2023-06-06 14:02:16 -0500

    Tween patience bar

--- Scripts/UI/PatienceBar.gd
+++ Scripts/UI/PatienceBar.gd
@@ -1,6 +1,8 @@
 extends Node3D
 class_name PatienceBar
 
+@export var smooth = true
+
 @onready var bar : Sprite3D = $BarPivot/Bar
 @onready var icon : Sprite3D = $Icon
 @onready var bar_pivot : Node3D = $BarPivot
@@ -21,5 +23,19 @@ func hide_visual():
 
 func _on_patience_changed(patience: float):
 	var clamped_patience = clamp(patience, 0, 1.0)
-	bar_pivot.scale.y = clamped_patience
-	bar.modulate = patience_gradient.sample(1.0 - clamped_patience)
+	var color = patience_gradient.sample(1.0 - clamped_patience)
+	
+	if smooth:
+		smooth_change(clamped_patience, color)
+	else:
+		bar_pivot.scale.y = clamped_patience
+		bar.modulate = color
+
+func smooth_change(patience: float, color: Color):
+	if patience < bar_pivot.scale.y and patience >= 0:
+		var tween = create_tween()
+		tween.tween_property(bar_pivot, "scale:y", patience, NetworkedPartyManager.patience_tick_rate_seconds).set_ease(Tween.EASE_OUT)
+		tween.tween_property(bar, "modulate", color, NetworkedPartyManager.patience_tick_rate_seconds)
+	else:
+		bar_pivot.scale.y = patience
+		bar.modulate = color
