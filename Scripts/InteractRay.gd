2de7ceba70e0d6340d734aaa33805510fbc06217
Tyler Walton <tyler.walton1452@gmail.com>
2023-06-19 01:24:21 -0500

    somewhat decent outline

--- Scripts/InteractRay.gd
+++ Scripts/InteractRay.gd
@@ -1,15 +1,16 @@
 extends RayCast3D
 
-@export var highlight_enabled = false
+@export var highlight_enabled = true
 
-#var outline_material : ShaderMaterial = preload("res://Materials/outline_material.tres")
-var outline_material : StandardMaterial3D = preload("res://Materials/Outline_mat.tres")
-var current_material : BaseMaterial3D = null
+var outline_material : StandardMaterial3D = preload("res://Materials/Grow_Outline_mat.tres")
 var looking_at : Interactable = null
+#var current_material : BaseMaterial3D = null
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_end"):
 		highlight_enabled = !highlight_enabled
+		if looking_at:
+			hide_outline(looking_at)
 
 func _physics_process(_delta):
 	if not highlight_enabled:
@@ -32,12 +33,15 @@ func _physics_process(_delta):
 
 func show_outline(interactable: Interactable):
 	if interactable.mesh_to_highlight != null:
-		current_material = interactable.mesh_to_highlight.get_active_material(0)
-		current_material.next_pass = outline_material
-		#interactable.mesh_to_highlight.material_override = outline_material
+		interactable.mesh_to_highlight.material_overlay = outline_material
+		
+#		current_material = interactable.mesh_to_highlight.get_active_material(0)
+#		interactable.mesh_to_highlight.set_surface_override_material(0, outline_material)
+#		interactable.mesh_to_highlight.material_override = outline_material
 
 func hide_outline(interactable: Interactable):
 	if interactable.mesh_to_highlight != null:
-		#interactable.mesh_to_highlight.material_override = current_material
-		current_material.next_pass = null
-	current_material = null
+		interactable.mesh_to_highlight.material_overlay = null
+#		interactable.mesh_to_highlight.set_surface_override_material(0, current_material)
+
+#	current_material = null
