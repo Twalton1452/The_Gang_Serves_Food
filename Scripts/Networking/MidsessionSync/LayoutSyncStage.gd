0b1a08a2b26dce608c05ee16ecc5d4c8fa33c577
Tyler Walton <tyler.walton1452@gmail.com>
2023-06-23 15:11:01 -0500

    NetworkedGrouperNode3Ds are used to sync non-networked nodes

--- Scripts/Networking/MidsessionSync/LayoutSyncStage.gd
+++ Scripts/Networking/MidsessionSync/LayoutSyncStage.gd
@@ -8,24 +8,44 @@ func _ready():
 	batch_size = LAYOUT_BATCH_SIZE
 
 func _nodes_to_sync() -> Array[Node]:
-	return get_tree().get_nodes_in_group("runtime_spawned")
+	return get_tree().get_nodes_in_group(NetworkedGrouperNode3D.GROUP_NAME)
 
+## Each Write is for a Parent Node's children
 func _write_node(node: Node, writer: ByteWriter) -> void:
-	writer.write_str(node.scene_file_path)
-	writer.write_path_to(node.get_parent())
+	var sync_nodes : Array[Node] = (node as NetworkedGrouperNode3D).nodes
 	
-	writer.write_str(node.name)
-	writer.write_vector3(node.global_position)
-	writer.write_vector3(node.global_rotation)
+	writer.write_path_to(node)
+	writer.write_int(sync_nodes.size())
+	for sync_node in sync_nodes:
+		writer.write_str(sync_node.name)
+		writer.write_str(sync_node.scene_file_path)
+		writer.write_vector3(sync_node.global_position)
+		writer.write_vector3(sync_node.global_rotation)
 
+## Each Read is for a parent Node's children
 func _read_node(reader: ByteReader) -> void:
-	var scene_path = reader.read_str()
-	var parent_path_to = reader.read_path_to()
+	var parent_path = reader.read_path_to()
+	var num_nodes = reader.read_int()
 	
-	var node_name = reader.read_str()
-	var global_pos = reader.read_vector3()
-	var global_rot = reader.read_vector3()
-	var spawned_node = NetworkingUtils.spawn_node_by_scene_path(scene_path, get_node(parent_path_to))
-	spawned_node.name = node_name
-	spawned_node.global_position = global_pos
-	spawned_node.global_rotation = global_rot
+	var grouper_node : NetworkedGrouperNode3D = get_node(parent_path)
+	var to_delete_nodes : Array[Node] = grouper_node.nodes
+	
+	for _i in range(num_nodes):
+		var node_name = reader.read_str()
+		var scene_path = reader.read_str()
+		var global_pos = reader.read_vector3()
+		var global_rot = reader.read_vector3()
+		
+		var node : Node = grouper_node.get_node_or_null(node_name)
+		if node == null:
+			node = NetworkingUtils.spawn_node_by_scene_path(scene_path, grouper_node)
+		elif to_delete_nodes.size() > 0:
+			var index = to_delete_nodes.find(node)
+			to_delete_nodes.remove_at(index)
+		
+		node.name = node_name
+		node.global_position = global_pos
+		node.global_rotation = global_rot
+	
+	for to_delete in to_delete_nodes:
+		to_delete.queue_free()
---
+++ Scripts/Networking/NetworkedGrouperNode3D.gd
@@ -0,0 +1,31 @@
+@icon("res://Icons/group.svg")
+extends Node3D
+class_name NetworkedGrouperNode3D
+
+## Class to add/remove nodes with network safe names to a group for syncing purposes
+
+const GROUP_NAME = "Groupers"
+const CHILD_NAMING_FORMAT = "%s_%d"
+
+## Snap the Node to this value when player's are moving it around
+## Adjustable to account for non-uniformly scaled Nodes
+@export var snapping_spacing = Vector3(0.05, 0.05, 0.05)
+
+var ID = 0
+var nodes : Array[Node] : get = get_nodes
+
+func get_nodes() -> Array[Node]:
+	return get_children()
+
+func _enter_tree():
+	child_entered_tree.connect(_on_child_entered_tree)
+
+func _ready():
+	add_to_group(GROUP_NAME)
+
+func _on_child_entered_tree(node: Node) -> void:
+	generate_network_safe_name_for(node)
+
+func generate_network_safe_name_for(node: Node) -> void:
+	node.name = CHILD_NAMING_FORMAT % [node.name, ID]
+	ID += 1
